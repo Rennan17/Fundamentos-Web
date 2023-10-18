82a765ce81558a8f4231d861543d47d0b04092dd
rennan17 <rsmoura252@gmail.com>
2023-10-17 21:27:40 -0300

    Inicio de uso Javascript

---
+++ Aula 01/Aula 01  Aplicando conhecimentos/script.js
@@ -0,0 +1,18 @@
+function dados() {
+    var nomeCompleto = "Rennan de Souza Moura";
+    var ano = 2023;
+    var twitter = "@RennanMackenzie"
+    var facebook = "@RennanMackenzie"
+    var instagram = "@RennanMackenzie"
+    var linkedln = "@RennanMackenzie"
+    var sexo = "Masculino"
+    var polo = "Belenzinho"
+
+    var dadosCompletos = "Nome Completo: " + nomeCompleto + "</br>Ano: " + ano + "<" +
+            "/br> Twitter: " + twitter + "</br> Facebook: " + facebook +
+            "</br> Instagram: " + instagram + "</br> Linkedln: " + linkedln +
+            "</br> Sexo: " + sexo + "</br> Polo: " + polo;
+    document.write(dadosCompletos);
+}
+
+dados();
